ede0cc8b0a0e7c3e30aac186c4b6234722ba2713
Gimmers <shanemassaad@hotmail.com>
2018-07-21 15:26:05 +0100

    Removed cached files

--- commands/serverinfo.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: 'serverinfo',
-	description: 'Information about the server',
-	guildOnly: true,
-	cooldown: 5,
-	execute(message, args) {
-		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-	},
-};
